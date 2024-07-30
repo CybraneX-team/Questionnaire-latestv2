b3f404f2a0068dacb1565df9a3de382a8ba7c730
SAIYAM1243 <109846906+SAIYAM1243@users.noreply.github.com>
2024-07-30 16:31:50 +0530

    commoncomponent next button fix

--- src/components/CommonComponent.js
+++ src/components/CommonComponent.js
@@ -3,6 +3,7 @@ import { Box, Button } from "@mui/material";
 import ProgressCircles from "./ProgressCircles";
 import BarChart from "./BarChart";
 import RadarChart from "./radarChart";
+import "./styles.css";
 
 const CommonComponent = ({ handleNext, section }) => {
   return (
@@ -41,7 +42,7 @@ const CommonComponent = ({ handleNext, section }) => {
         <ProgressCircles />
         {/* <RadarChart /> */}
       </Box>
-      <Button
+      {/* <Button
         variant="contained"
         onClick={handleNext}
         style={{
@@ -58,7 +59,31 @@ const CommonComponent = ({ handleNext, section }) => {
         }}
       >
         Next
-      </Button>
+      </Button> */}
+      <button
+        className="next-button"
+        variant="contained"
+        onClick={handleNext}
+        style={{
+          marginTop: "20px",
+        }}
+      >
+        <span className="label">Next</span>
+        <span className="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path
+              fill="currentColor"
+              d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+            ></path>
+          </svg>
+        </span>
+      </button>
     </Box>
   );
 };
